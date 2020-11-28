4b261861ffe068d43736e9804a28cbd2d9037a84
burdakin <serjb94@gmail.com>
2020-11-28 14:52:57 +0300

    add function test

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function add() {
-var todo_text = prompt('введите текст задачи');
-document.getElementById('todo').innerHTML = '<h1>О, привет, пидор! Вот твоя задача</h1>' + todo_text;
-}
+
+function save(id_name) {
+    let todo_in_text = document.getElementById(id_name).value;
+    let new_todo = document.createElement('div');
+    new_todo.className = 'new_todo';
+    new_todo.innerHTML = '<b>' + todo_in_text + '</b>';
+    document.body.append(new_todo);
+};
+
+
