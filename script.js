2f1715218cb0cbc7a7b01361f48879f7b73cf94a
burdakin <serjb94@gmail.com>
2020-12-03 08:47:15 +0300

    Restored functionality, added non-working functions as a comment.

--- script.js
+++ script.js
@@ -13,39 +13,27 @@ function render (){
     delete_old_render()
     array_id = 1;
     for (let i=0; i<todo_array.length; i++) {
+        let new_todo = document.createElement('div');
+        new_todo.className = 'new_todo';
         let array_data_id = todo_array[i].id_num;
         let array_data_text = todo_array[i].todo_parse;
-        let new_todo = document.createElement('div');
-        new_todo.id = 'new_todo';
-        let del_btn = add_del_btn(i);
-        new_todo.innerHTML = array_id++ + ': ' + array_data_text + ": " + del_btn;
+        new_todo.innerHTML = '<b>' + array_id++ + ': ' + array_data_text + '</b>';
         document.body.append(new_todo);
-
     }
 }
 
-function delete_todo(num) {
-    /*alert('э, ты чё?');
-    let delete_num = +prompt('какой номер задачи ты хочешь удалить?');*/
-    todo_array.splice(num,1);
+function delete_todo() {
+    alert('э, ты чё?');
+    let delete_num = +prompt('какой номер задачи ты хочешь удалить?');
+    todo_array.splice(delete_num,1);
     render();
     storage();
 }
 
-//чот из-за смены классов на айди перестала работать эта функция//
 function delete_old_render() {
-    document.querySelectorAll('#new_todo').forEach(function (a) {
+    document.querySelectorAll('.new_todo').forEach(function (a) {
     a.remove();}
     )
-}
-
-function add_del_btn(num) {
-    let todo_num = num;
-    let new_del_btn = document.createElement('button');
-    new_del_btn.id = 'del_btn';
-    document.getElementById('new_todo').append(new_del_btn);
-
-    /*new_del_btn.onclick = delete_todo(todo_num);*/
 }
 
 function edit_todo() {
@@ -68,12 +56,13 @@ function storage() {
 
 function render_from_storage() {
     todo_array = (JSON.parse(window.localStorage.getItem('todo_list')));
+    array_id = 1;
     if (todo_array !== null) {
         //Сюда вставил кусок кода из render(), так как выпадала ошибка из-за функции удаления старого рендера//
         for (let i = 0; i < todo_array.length; i++) {
             let new_todo = document.createElement('div');
             new_todo.className = 'new_todo';
-            /*let array_data_id = todo_array[i].id_num;*/
+            let array_data_id = todo_array[i].id_num;
             let array_data_text = todo_array[i].todo_parse;
             new_todo.innerHTML = '<b>' + array_id++ + ': ' + array_data_text + '</b>';
             document.body.append(new_todo);
@@ -86,4 +75,38 @@ function render_from_storage() {
 function lstorage_clr() {
     delete_old_render();
     window.localStorage.clear();
+    todo_array = [];
 }
+
+/* Здесь мои потуги сделать функции для добавления кнопок
+
+function add_del_btn(num) {
+    let todo_num = num;
+    let new_del_btn = document.createElement('button');
+    new_del_btn.id = 'del_btn';
+    document.getElementById('new_todo').append(new_del_btn);
+    new_del_btn.onclick = delete_todo(todo_num);
+}
+
+function render (){
+    delete_old_render()
+    array_id = 1;
+    for (let i=0; i<todo_array.length; i++) {
+        let array_data_id = todo_array[i].id_num;
+        let array_data_text = todo_array[i].todo_parse;
+        let new_todo = document.createElement('div');
+        new_todo.id = 'new_todo';
+        let del_btn = add_del_btn(i);
+        new_todo.innerHTML = array_id++ + ': ' + array_data_text + ": " + del_btn;
+        document.body.append(new_todo);
+    }
+}
+
+function delete_todo(num) {
+    alert('э, ты чё?');
+    todo_array.splice(num,1);
+    render();
+    storage();
+}
+
+*/
