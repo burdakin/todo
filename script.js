1e89768677bebca838ada138e0a7a73c3dbb4451
burdakin <serjb94@gmail.com>
2020-12-05 01:10:02 +0300

    Button Delete is now deleting (incorrectly)

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 var todo_array = [];
 var array_id = 0;
+var del_num = 0;
 
 function add_to_array() {
-    let todo_parse = document.getElementById('todo_text').value;
     let id_num = array_id++;
+    let todo_parse = document.getElementById('todo_text').value;
     todo_array.push({id_num,todo_parse});
-    render();
     storage();
+    render();
+
 }
 
 function render (){
     delete_old_render()
-    array_id = 1;
     for (let i=0; i<todo_array.length; i++) {
         let new_todo = document.createElement('div');
         new_todo.className = 'new_todo';
         let array_data_id = todo_array[i].id_num;
         new_todo.id = array_data_id;
+        del_num = array_data_id;
         let array_data_text = todo_array[i].todo_parse;
-        new_todo.innerHTML = '<b>' + array_id++ + ': ' + array_data_text + '</b>';
+        new_todo.innerHTML = '<b>' + array_data_id++ + ': ' + array_data_text + '</b>';
         document.body.append(new_todo);
-        add_del_btn(array_data_id);
+        add_del_btn(del_num);
     }
 }
 
@@ -34,18 +36,6 @@ function delete_old_render() {
     }
 }
 
-function edit_todo() {
-    let edit_num = +prompt('какой номер хочешь исправить?');
-    let edit_todo = todo_array[edit_num].todo_parse;
-    let edit_new_todo = prompt('введи новую задачу вместо ' + edit_todo);
-    //вот здесь не знаю, нужно ли это, но вставил на всякий случай, чтобы имена переменных и ключи объектов совпали//
-    let id_num = edit_num;
-    let todo_parse = edit_new_todo;
-    delete todo_array[edit_num];
-    todo_array.splice(edit_num,1,{id_num,todo_parse});
-    render();
-    storage();
-}
 
 function storage() {
     let store_todo = JSON.stringify(todo_array);
@@ -62,10 +52,11 @@ function render_from_storage() {
             new_todo.className = 'new_todo';
             let array_data_id = todo_array[i].id_num;
             new_todo.id = array_data_id;
+            del_num = array_data_id;
             let array_data_text = todo_array[i].todo_parse;
-            new_todo.innerHTML = '<b>' + array_id++ + ': ' + array_data_text + '</b>';
+            new_todo.innerHTML = '<b>' + array_data_id++ + ': ' + array_data_text + '</b>';
             document.body.append(new_todo);
-            add_del_btn(array_data_id);
+            add_del_btn(del_num);
         }
     } else {
         todo_array = []
@@ -87,54 +78,15 @@ function lstorage_clr() {
 function add_del_btn(num) {
     let new_del_btn = document.createElement('button');
     new_del_btn.value = "delete!";
-    new_del_btn.innerHTML = 'oodalee'
     new_del_btn.id = 'del_btn';
-    //new_del_btn.onclick = delete_todo(document.getElementById('num'));//
+    new_del_btn.setAttribute('onclick', 'delete_todo'+'(' + num + ')');
+    new_del_btn.innerHTML = 'oodalee'
     document.getElementById(num).append(new_del_btn);
 }
 
 function delete_todo(del_num) {
     todo_array.splice(del_num,1);
+    array_id = 0;
     render();
     storage();
 }
-
-/* ненужное
-
-function add_del_btn(num) {
-    let todo_num = num;
-    let new_del_btn = document.createElement('button');
-    new_del_btn.id = 'del_btn';
-    document.getElementById('new_todo').append(new_del_btn);
-    new_del_btn.onclick = delete_todo(todo_num);
-}
-
-function render (){
-    delete_old_render()
-    array_id = 1;
-    for (let i=0; i<todo_array.length; i++) {
-        let array_data_id = todo_array[i].id_num;
-        let array_data_text = todo_array[i].todo_parse;
-        let new_todo = document.createElement('div');
-        new_todo.id = 'new_todo';
-        let del_btn = add_del_btn(i);
-        new_todo.innerHTML = array_id++ + ': ' + array_data_text + ": " + del_btn;
-        document.body.append(new_todo);
-    }
-}
-
-function delete_todo(num) {
-    alert('э, ты чё?');
-    todo_array.splice(num,1);
-    render();
-    storage();
-}
-
-function delete_todo() {
-    alert('э, ты чё?');
-    let delete_num = +prompt('какой номер задачи ты хочешь удалить?');
-    todo_array.splice(delete_num-1,1);
-    render();
-    storage();
-}
-*/
